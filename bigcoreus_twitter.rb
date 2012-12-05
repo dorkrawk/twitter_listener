0423f8f4e27241ae2649e2ab8e6cb50eb6beafa7
Dave <dave.schwantes@gmail.com>
2012-12-04 20:43:31 -0800

    readding eventmachine loop

--- bigcoreus_twitter.rb
+++ bigcoreus_twitter.rb
@@ -4,23 +4,23 @@ require_relative 'twitter_listener'
 
 $stdout.sync = true    # this exists so I'll get output from Sinatra
 
-listener_researt_period = 15 * 60 # 15 minutes
+listener_researt_period = 2 * 60 # 15 minutes
 
-client = TwitterListener.new
-puts "starting Twitter listener..."
-client.restart
-puts "listening to Twitter..."
+# client = TwitterListener.new
+# puts "starting Twitter listener..."
+# client.restart
+# puts "listening to Twitter..."
 
-#EM.run do 
-#	client = TwitterListener.new
-#	puts "starting Twitter listener..."
-#	client.restart
-#	puts "listening to Twitter..."
+EM.run do 
+	client = TwitterListener.new
+	puts "starting Twitter listener..."
+	client.restart
+	puts "listening to Twitter..."
 
-#	EM::PeriodicTimer.new(listener_researt_period) do 
-#		puts "restarting Twitter listener"
-#		client.restart
-#	end
-#end
+	EM::PeriodicTimer.new(listener_researt_period) do 
+		puts "restarting Twitter listener"
+		client.restart
+	end
+end
 
 
